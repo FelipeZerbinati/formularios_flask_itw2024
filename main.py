1e68204a306ddde1c1ce362ac18c3f6f1fda9e01
Douglas Mac M1 <douglashsabreu@gmail.com>
2024-09-01 19:48:28 -0300

    home page fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return 'Hello, World!'
+    frutas = ["maçã", "banana", "laranja", "melancia", "uva", "mamão", "abacaxi"]
+    return render_template("index.html", frutas_do_html=frutas)
 
 
 if __name__ == '__main__':
