e9df8f0418420c01fa953552205069321e07ee42
FelipeZerbinati <fezerba@gmail.com>
2024-09-02 09:34:20 -0300

    Aula 02/09/2024

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+frutas = ["maçã", "banana", "laranja", "melancia", "uva", "mamão", "abacaxi"]
 
-@app.route('/')
+@app.route('/', methods = {'GET', 'POST'})
 def home_page():
-    frutas = ["maçã", "banana", "laranja", "melancia", "uva", "mamão", "abacaxi"]
+    
+    if request.method == 'POST':
+        if request.form.get('fruta'):
+            frutas.append(request.form.get("fruta"))
+
     return render_template("index.html", frutas_do_html=frutas)
 
 
