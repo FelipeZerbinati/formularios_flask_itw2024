4fdad04b1cc3b6cec8823966732e32d52ee11033
FelipeZerbinati <fezerba@gmail.com>
2024-09-02 18:23:16 -0300

    Tarefa 02 09 2024

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, request
+#Arquivo main.py
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
 frutas = ["maçã", "banana", "laranja", "melancia", "uva", "mamão", "abacaxi"]
 
-@app.route('/', methods = {'GET', 'POST'})
+@app.route('/')
 def home_page():
-    
-    if request.method == 'POST':
-        if request.form.get('fruta'):
-            frutas.append(request.form.get("fruta"))
-
     return render_template("index.html", frutas_do_html=frutas)
+
+
+@app.route('/inserir_frutas', methods=['GET', 'POST'])
+def second_page():
+    if request.method == 'POST':
+        nova_fruta = request.form['fruta']
+        frutas.append(nova_fruta)
+        return redirect(url_for('home_page'))
+    return render_template("index2.html")
 
 
 if __name__ == '__main__':
